d463bd243a7b3e69246d557edaf0e55e58c8542b
Naeem-Ahmad <naeemsiddique574@gmail.com>
2025-04-07 12:56:58 +0500

    Second Commit

--- index.php
+++ index.php
@@ -11,6 +11,17 @@
         <div class="card">
             <div class="body">
                 <h4>Card body</h4>
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Document</title>
+                </head>
+                <body>
+                    
+                </body>
+                </html>
             </div>
         </div>
     </div>
